278e4eb8d523f8f09c90b47b834fa2b32985528e
Ian McKellar <ian@mckellar.org>
2011-12-20 08:52:28 -0800

    add playback token for rdio.github.com

--- tokens.js
+++ tokens.js
@@ -1,4 +1,5 @@
 var tokens = {
   'localhost': 'GAlNi78J_____zlyYWs5ZG02N2pkaHlhcWsyOWJtYjkyN2xvY2FsaG9zdEbwl7EHvbylWSWFWYMZwfc=',
-  'scratch.ianloic.com': 'GBNO8ELe_____3d5a3dwN3l4ejNmY2M2Ymhhd2h0eXV4Y3NjcmF0Y2guaWFubG9pYy5jb22mytMPh9MPeSOr2a8E-mVR'
+  'scratch.ianloic.com': 'GBNO8ELe_____3d5a3dwN3l4ejNmY2M2Ymhhd2h0eXV4Y3NjcmF0Y2guaWFubG9pYy5jb22mytMPh9MPeSOr2a8E-mVR',
+  'rdio.github.com': 'GA9O8L0b_____3d5a3dwN3l4ejNmY2M2Ymhhd2h0eXV4Y3JkaW8uZ2l0aHViLmNvbequdEETmQWPjaee9gOyquQ='
 };
