92893e3fdf2462b28811de9ebb0c7e242228d08a
Ian McKellar <ian@mckellar.org>
2011-12-20 12:11:33 -0800

    fix up tokens

--- tokens.js
+++ tokens.js
@@ -1,5 +1,4 @@
 var tokens = {
-  'localhost': 'GAlNi78J_____zlyYWs5ZG02N2pkaHlhcWsyOWJtYjkyN2xvY2FsaG9zdEbwl7EHvbylWSWFWYMZwfc=',
-  'scratch.ianloic.com': 'GBNO8ELe_____3d5a3dwN3l4ejNmY2M2Ymhhd2h0eXV4Y3NjcmF0Y2guaWFubG9pYy5jb22mytMPh9MPeSOr2a8E-mVR',
-  'rdio.github.com': 'GA9O8L0b_____3d5a3dwN3l4ejNmY2M2Ymhhd2h0eXV4Y3JkaW8uZ2l0aHViLmNvbequdEETmQWPjaee9gOyquQ='
+  'localhost': 'GAlO8Ou______3MyajZoNGpudXhxdHg3YmFxYmFqdmt6dWxvY2FsaG9zdLdLSB70cWKIF6Rfx27MQ4Q=',
+  'rdio.github.com': 'GA9O8Ou5_____3MyajZoNGpudXhxdHg3YmFxYmFqdmt6dXJkaW8uZ2l0aHViLmNvbW2v8A-b5v65KiNTkYiiReg='
 };
