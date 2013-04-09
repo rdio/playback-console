8cd695ed21aea2e7e3b3d2ba56f887bd6ec894cb
Devin Sevilla <dasevilla@gmail.com>
2013-04-09 12:09:34 -0700

    Support rdio.github.io

--- tokens.js
+++ tokens.js
@@ -1,4 +1,5 @@
 var tokens = {
   'localhost': 'GAlO8Ou______3MyajZoNGpudXhxdHg3YmFxYmFqdmt6dWxvY2FsaG9zdLdLSB70cWKIF6Rfx27MQ4Q=',
-  'rdio.github.com': 'GA9O8Ou5_____3MyajZoNGpudXhxdHg3YmFxYmFqdmt6dXJkaW8uZ2l0aHViLmNvbW2v8A-b5v65KiNTkYiiReg='
+  'rdio.github.com': 'GA9O8Ou5_____3MyajZoNGpudXhxdHg3YmFxYmFqdmt6dXJkaW8uZ2l0aHViLmNvbW2v8A-b5v65KiNTkYiiReg=',
+  'rdio.github.io': 'GA5RZGc-_____2R2cHlzNHd5ZXg3Z2M0OXdoaDY3aHdrbnJkaW8uZ2l0aHViLmlvfSrb4OkirIFEIwe9aWFw8Q=='
 };
